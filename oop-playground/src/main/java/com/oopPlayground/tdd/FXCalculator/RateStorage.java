3d0e3011e4179e1915f62b0a6d25d9ebf29aa99f
Abhimanyu Chakravarty <viperzenator@gmail.com>
2013-08-07 13:07:40 +0530

    Added logic to understand mockito and mocking in general

---
+++ oop-playground/src/main/java/com/oopPlayground/tdd/FXCalculator/RateStorage.java
@@ -0,0 +1,12 @@
+package com.oopPlayground.tdd.FXCalculator;
+
+public class RateStorage {
+
+    public int getConversionRate(String sourceCurrency, String destinationCurrency) {
+        return 0;
+    }
+
+    public void storeRate(String sourceCurrency, String destinationCurrency, int conversionRate) {
+
+    }
+}
